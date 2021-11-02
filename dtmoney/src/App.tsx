118f8da6223a486a2c8e10e22b68c369244b2f6a
RaimundooNeto <raimundononatobatistaneto@gmail.com>
2021-11-01 23:01:55 -0300

    Finalização da aplicação dtmoney

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Modal from 'react-modal';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { TransactionsProvider } from "./hooks/useTransactions";
 import { NewTransactionModal } from "./components/NewTransactionModal";
 
 import { GlobalStyle } from "./styles/global";
@@ -21,7 +22,7 @@ export function App() {
 
 
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
       <NewTransactionModal 
@@ -29,6 +30,6 @@ export function App() {
       onRequestClose={handleCloseNewTransactionModal}
       />
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
